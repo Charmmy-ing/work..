235a782dc5c3fa72512c69663ae18ec634e3e2b0
Charmmmy <2127137114@qq.com>
2025-11-09 20:41:43 +0800

    Add files via upload

---
+++ 韩信/韩信/韩信.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+int main()
+{
+	int max;
+	printf("max=");
+	scanf_s("%d", &max);
+	int number;
+	for (number = 1; number <=max; ++number)
+	{
+		if (number % 3 == 2 && number % 5 == 3 && number % 7 == 2)
+		{
+			printf("%d\n", number);
+		}
+	}
+	
+	return 0;
+}
